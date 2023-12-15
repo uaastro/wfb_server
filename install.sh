ce8ede9d66112efdfe38243a1a0867f23c8bebb9
uaastro <93040300+uaastro@users.noreply.github.com>
2023-12-15 17:37:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 # Install required packages
 apt update
-
-apt install python3-all python3-all-dev libpcap-dev libsodium-dev python3-pip python3-pyroute2 \
+cp -r ./wfb_server /usr/sbin/
+cp ./wfb_server/wfb.service /etc/systemd/system/apt install python3-all python3-all-dev libpcap-dev libsodium-dev python3-pip python3-pyroute2 \
   python3-future python3-twisted python3-serial iw virtualenv debhelper dh-python build-essential -y
+
+pip install rich
+pip install click
 
 # Build
 make
 
 # Install
 
-# Create key and copy to right location
-./wfb_keygen
-cp *.key /etc/
-
-#cp wfb_tx /usr/bin/
-#cp wfb_rx /usr/bin/
-#cp wfb_keygen /usr/bin/
-
-cp wfb_* /usr/bin/
 cp -r ./wfb_server /usr/sbin/
 cp ./wfb_server/wfb.service /etc/systemd/system/
+
+# Create key and copy to right location
+#./wfb_keygen
+cp ./cfg/*.key /etc/
+
+cp wfb_tx /usr/bin/
+cp wfb_rx /usr/bin/
+cp wfb_keygen /usr/sbin/wfb_server/
+
+/usr/sbin/wfb_server/add_wlan wlx*
 
 cat <<EOF >> /etc/bash.bashrc
 export PATH=\$PATH:/usr/sbin/wfb_server
